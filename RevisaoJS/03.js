ceffddce3439f9dad40c7074b36114c61b093107
Ana-LaurCN <ana.l.nascimento@iesb.edu.br>
2025-03-06 22:03:56 -0300

    versão final

--- RevisaoJS/03.js
+++ RevisaoJS/03.js
@@ -3,10 +3,16 @@ let idade = 70;
 //menor de idade
 
 function verifIdade(idade){
+
+    //menor de idade
     if (idade < 18){
         console.log ("---- A pessoa é menor de idade");
+
+    //maioe de idade
     } else if (idade >= 18 && idade < 65){
         console.log ("---- A pessoa é maior de idade");
+
+    //idosa
     } else if (idade >= 65){
         console.log ("---- A pessoa é idosa");
     }
