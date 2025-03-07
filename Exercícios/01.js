a6defd2e6c65195a4590fe0337b75a2eea00b226
Ana-LaurCN <ana.l.nascimento@iesb.edu.br>
2025-03-06 21:24:35 -0300

    Exercício 1: Manipulação de Strings

--- Exercícios/01.js
+++ Exercícios/01.js
@@ -1,1 +1,24 @@
-a
+let antes = "palavra";
+let letraAntiga = "a";
+let letraNova = "3";
+
+function palavra(plv, lentraAntiga, letraNova) {
+
+    //1. Maiuscula
+    let maiusculas = plv.toUpperCase();
+    console.log("Conversão para maiúscula: ", maiusculas);
+
+    //2. minuscula
+    let minusculas = plv.toLowerCase();
+    console.log("Conversão para minúsculas: ", minusculas);
+
+    //3. Inverter
+    let invertida = plv.split('').reverse().join('');
+    console.log("Invertida:", invertida);
+
+     // 4. Substituir
+     let substituida = plv.split(letraAntiga).join(letraNova);
+     console.log("Substituída:", substituida);
+}
+
+palavra(antes, letraAntiga, letraNova);
